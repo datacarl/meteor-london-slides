a5ebf97f60a065d880f0898eb895bd5c58730ea9
Carl Littke <carl.littke@gmail.com>
2014-06-11 18:53:49 +0200

    Meteor london.

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Lookback.io<br>Built with Meteor',
+    subtitle: 'A demo of Lookback and a peek into the life of a list view.',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -12,7 +12,7 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/favicon-16.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Carl Littke',
+    company: 'Co-founder and web dev<br>Lookback',
+    // gplus: 'http://plus.google.com/1234567890',
+    twitter: '@CarlLittke',
+    www: 'http://datacarl.com',
+    github: 'http://github.com/datacarl'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
